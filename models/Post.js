bbfdd0e98400d8c197c3a65c0436cd626b1f8b0d
devender kumar <119566781+devender9695@users.noreply.github.com>
2024-05-09 09:56:55 +0530

    Create Post.js

---
+++ models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  comments: [
+    {
+      userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model("Post", PostSchema);
