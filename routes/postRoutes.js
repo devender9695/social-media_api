86e0dca784e401c1da95a0caefae9ea179216cd8
devender kumar <119566781+devender9695@users.noreply.github.com>
2024-05-18 17:18:54 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const checkUserAuth = require("../middlewares/auth.js");
 const postController = require("../controllers/postcontroller");
 
-router.post("/create", postController.createPost);
-router.get("/:postId", postController.getPost);
-router.put("/:postId", postController.updatePost);
-router.post("/like/:postId", postController.likePost);
-router.post("/comment/:postId", postController.addComment);
-router.delete("/:postId", postController.deletePost);
-
+router.post("/create", checkUserAuth, postController.createPost);
+router.get("/:postId", checkUserAuth, postController.getPost);
+router.put("/:postId", checkUserAuth, postController.updatePost);
+router.post("/like/:postId", checkUserAuth, postController.likePost);
+router.post("/comment/:postId", checkUserAuth, postController.addComment);
+router.delete("/:postId", checkUserAuth, postController.deletePost);
 
 module.exports = router;
